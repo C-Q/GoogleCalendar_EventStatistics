12dd031646a610cd2eb63de13cff92451d56c5a5
C-Q <podzemliu@gmail.com>
2019-11-10 16:17:59 +0200

    Что-то изменил

--- EventsCalStat.js
+++ EventsCalStat.js
@@ -1,12 +1,7 @@
 function myFunction() {
-  
-  // This version gets some range event objects, and returns arrays with:
-  //   Work days in the each month
-  //   Events in the each month
-  //   Average amount per month
-  
+  // это хуй пойми шо, сохранено для истории )))))
   var cal = CalendarApp.getCalendarById("kging7ubddkgvkgu6k12iuohi4@group.calendar.google.com");
-  var events = cal.getEvents(new Date("October 1, 2018 00:00:00 CST"), new Date("December 28, 2018 00:00:00 CST"));
+  var events = cal.getEvents(new Date("December 28, 2018 14:00:00"), new Date("January 3, 2019 13:00:00"));
   
 //  var titles = [];
 //  titles.push(events[0].getTitle()); 
@@ -16,44 +11,79 @@ function myFunction() {
 //  var endTime = (events[events.length-1].getEndTime().getHours());
 //  Logger.log('Start/end time last event in the range: ' + startTime + ' ' + endTime);
   
-  var daysEachMonth = [];                 // Work days in the each month:  21, 23, 20
-  var eventsEachMonth = [];               // Events in the each month:  85, 139, 82
-  var averageEachMonth =[];               // Average amount per month:  4.05 ,6.04, 4.10
+  var daysEachMonth = [];                   // Work days in the each month
+  var eventsEachMonth = [[]];               // Events in the each month
+  var averageEachMonth =[[]];               // Average amount per month
+  var year_totalEveryYear = [[],[]];        // box for this yearValue[0], and amount of all events in this year[1]
+  var arrYear = [];                         // result array for writing in the sheet
+  var enevtDescr = [];                        // for debugging
+  
+  
   var earlyDateVal = events[0].getStartTime().getDate();
   var earlyMonthVal = events[0].getStartTime().getMonth();
+  var earlyYearVal = events[0].getStartTime().getFullYear();
   var daysInMonth = 1;
   var eventsInMonth = 0;
+  var eventsInYear = 0;
   
   for (var i=0; i<events.length; i++) {
-    var nextMonthVal = events[i].getStartTime().getMonth();
-    if (nextMonthVal == earlyMonthVal){
-      eventsInMonth ++;
-      
-      var nextDateVal = events[i].getStartTime().getDate();
-      if(nextDateVal != earlyDateVal){
-        daysInMonth ++;
-        earlyDateVal = events[i].getStartTime().getDate();
-      }
-      
+    enevtDescr.push( events[i].getTitle() + ' | Date-' + events[i].getStartTime().getDate() + '; startTime-' +  events[i].getStartTime().getHours() + ':00 ' );
+    var nextYearVal = events[i].getStartTime().getFullYear();
+    if (nextYearVal == earlyYearVal){
+      eventsInYear ++;
+    
+      var nextMonthVal = events[i].getStartTime().getMonth();
+      if (nextMonthVal == earlyMonthVal){
+        eventsInMonth ++;
+        
+        var nextDateVal = events[i].getStartTime().getDate();
+        if(nextDateVal != earlyDateVal){
+          daysInMonth ++;
+          earlyDateVal = events[i].getStartTime().getDate();
+        };
+      } else {
+        daysEachMonth.push(daysInMonth);
+        daysInMonth = 1;
+//        eventsEachMonth[0].push(eventsInMonth);
+        eventsInMonth = 1;
+        earlyMonthVal = events[i].getStartTime().getMonth();
+      };
     } else {
+      year_totalEveryYear[0][0] = earlyYearVal;
+      year_totalEveryYear[1][0] = eventsInYear;
+      eventsInYear = 0;
+      eventsEachMonth[0].push(eventsInMonth);
       daysEachMonth.push(daysInMonth);
+      earlyYearVal = events[i].getStartTime().getFullYear();
       daysInMonth = 1;
-      eventsEachMonth.push(eventsInMonth);
       eventsInMonth = 1;
-      earlyMonthVal = events[i].getStartTime().getMonth();
-    }
-  }
+      for (var j=0; j<daysEachMonth.length; j++){
+        var averageInMonth = (eventsEachMonth[0][j] / daysEachMonth[j]).toFixed(2);
+        averageEachMonth[0].push(averageInMonth);
+      };
+      arrYear.push([year_totalEveryYear, eventsEachMonth, averageEachMonth]);
+      daysEachMonth = [];
+      eventsEachMonth = [[]];
+      year_totalEveryYear = [[],[]];
+      
+    };
+  };
   daysEachMonth.push(daysInMonth);
-  eventsEachMonth.push(eventsInMonth);
-  
-  for (var j=0; j<eventsEachMonth.length; j++){
-    var averageInMonth = (eventsEachMonth[j] / daysEachMonth[j]).toFixed(2);
-    averageEachMonth.push(averageInMonth);
-  }
-  
-  Logger.log('Work days in the each month:  ' + daysEachMonth);
-  Logger.log('Events in the each month:  ' + eventsEachMonth);
-  Logger.log('Average amount per month:  ' + averageEachMonth);
+  eventsEachMonth[0].push(eventsInMonth);
+  year_totalEveryYear[0][0] = nextYearVal;
+  year_totalEveryYear[1][0] = eventsInYear;
+  eventsInYear = 0;
+  for (var j=0; j<daysEachMonth.length; j++){
+    var averageInMonth = (eventsEachMonth[0][j] / daysEachMonth[j]).toFixed(2);
+    averageEachMonth[0].push(averageInMonth);
+  };
+  arrYear.push([year_totalEveryYear, eventsEachMonth, averageEachMonth]);
+//  daysEachMonth = [];
+//  eventsEachMonth = [[]];
+//  year_totalEveryYear = [[],[]];
+  Logger.log('Result array:  ' + arrYear[0]);
+  Logger.log('Result array:  ' + arrYear[1]);
+  Logger.log('Events title:  ' + titleArr);
 }
-
+// Расставить точки останова и отдебажить пособытийно
 
